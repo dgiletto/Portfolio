22884b789ed0830aa4833e3722f18f56a6e3fa19
dgiletto <dgiletto@udel.edu>
2024-06-12 21:19:30 -0400

    Added Social Links

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../App.css';
 import Typewriter from '../Components/Typewriter';
 import Coder from "../Images/coding_avatar.png";
+import { Link } from "react-router-dom";
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
 import "animate.css";
 
 function Home() {
@@ -13,6 +16,18 @@ function Home() {
           My Name is <b>Dylan Giletto</b>
         </h1>
         <Typewriter />
+        <Link to="https://github.com/dgiletto">
+          <button>
+            Github
+            <FaGithub />
+          </button>
+        </Link>
+        <Link to="https://www.linkedin.com/in/dylan-giletto-775789269/">
+          <button>
+            Linkedin
+            <FaLinkedinIn />
+          </button>
+        </Link>
       </div>
       <div className= "animate__animated animate__bounceIn">
         <img className ="Coder" src={Coder} alt="" />
